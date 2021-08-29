455644361a5bdd0d975ff3120c846b82a2566196
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2021-08-29 15:36:11 +0100

    list posts from API

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,15 +1,46 @@
+import { useState } from 'react'
+import useFetch from '../hooks/useFetch'
+import Post from '../models/Post'
+import User from '../models/User'
 import PostPreview from './PostPreview'
 
 export default function PostList() {
+
+    const postsURL = `https://jsonplaceholder.typicode.com/posts`
+    const usersURL = `https://jsonplaceholder.typicode.com/users`
+
+    const {data: postList, headers: postHeaders, isLoading, errors} = useFetch<Post[]>(postsURL)
+    const {data: authorList} = useFetch<User[]>(usersURL)
+
+
+    const GetAuthorName = (authorId: number): string => {
+
+        const newAuthor = authorList?.find((author: any) => author.id == authorId)
+
+        return newAuthor?.name ?? "Unknown"
+    }
+
     return (
         <div className="container">
-            <PostPreview/>
-            <hr className="col-3 col-md-2 mb-5" />
-            <PostPreview/>
-            <hr className="col-3 col-md-2 mb-5" />
-            <PostPreview/>
-            <hr className="col-3 col-md-2 mb-5" />
-            <PostPreview/>
+            {isLoading && (<p>Loading ...</p>)}
+            {
+                (!isLoading && !errors) && (
+                    postList?.map(post => (
+                        <div className="container" key={post.id}>
+                            <PostPreview id={post.id} author={GetAuthorName(post.userId)} title={post.title} body={post.body} />
+                            <hr className="col-3 col-md-2 mb-5" />
+                        </div>
+                    ))
+                //        (<Pagination currentPage={currentPage} hasMore={hasMorePages} endpoint={''} />)
+                )
+            }
+
+            {errors && (
+                <div className="container">
+                    <h3>An error occurred</h3>
+                    <p>{errors}</p>
+                </div>
+            )}
         </div>
     )
 }
--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -1,15 +1,17 @@
-export default function PostPreview() {
+interface Props {id: number, author: string, title: string, body: string }
+
+export default function PostPreview({id, author, title, body }: Props) {
+    
     return (
         <article>
             <header className="mb-4">
-                <h1 className="fw-bolder mb-1">Welcome to Blog Post!</h1>
-                <div className="text-muted fst-italic mb-2">Posted on January 1, 2021 by Start Bootstrap</div>
-                <a className="badge bg-secondary text-decoration-none link-light" href="#!">Web Design</a>
-                <a className="badge bg-secondary text-decoration-none link-light" href="#!">Freebies</a>
+                <h1 className="fw-bolder mb-1">{title}</h1>
+                <div className="text-muted fst-italic mb-2">Posted by {author}</div>
+                <a className="badge bg-secondary text-decoration-none link-light" href="#!">Post</a>
             </header>
             <section className="mb-5">
-                <p className="fs-5 mb-4">Science is an enterprise that should be cherished as an activity of the free human mind. Because it transforms who we are, how we live, and it gives us an understanding of our place in the universe.</p>
-                <a href="#">Continue reading</a>
+                <p className="fs-5 mb-4">{body.substring(0, body.indexOf('\n'))}</p>
+                <a href={"/posts/" + id}>Continue reading</a>
             </section>
         </article>
     )
