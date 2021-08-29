0d14c2919c0b2a3bca98bf88b47923a0a2ef4136
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2021-08-29 14:37:52 +0100

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import CardAuthors from "./components/CardAuthors";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -6,20 +7,27 @@ import PostList from "./components/PostList";
 function App() {
   return (
     <div className="col-lg-8 mx-auto p-3 py-md-5">
-      <Header />
+      <Router>
 
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-8">
-            <PostList/>
-          </div>
-          <div className="col-lg-4">
-            <CardAuthors/>
+        <Header />
+
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8">
+              <Switch>
+                <Route path="/" exact> <PostList /> </Route>
+                <Route path="/page/:page"> <PostList/> </Route>
+              </Switch>
+            </div>
+            <div className="col-lg-4">
+              <CardAuthors />
+            </div>
           </div>
         </div>
-      </div>
 
-      <Footer />
+        <Footer />
+        
+      </Router>
     </div>
   );
 }
